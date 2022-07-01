e41e58cf4aa6c8cf83cc6fec2d78d5aa4e667c2b
devnoman01 <devnoman01@gmail.com>
2022-07-02 05:12:41 +0600

    calender page update complete

--- src/Pages/Calender/Calender.js
+++ src/Pages/Calender/Calender.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Calender = () => {
   return (
     <div className="max-w-6xl mx-auto">
       <div className="container mx-auto px-3 lg:px-0">
         <h1 className="text-3xl text-center font-medium my-7">Calender API</h1>
+        <div className="flex justify-center">
+          <Calendar />
+        </div>
       </div>
     </div>
   );
