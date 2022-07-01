5e3189757f5ce07d0d5df8304b87024b25375887
devnoman01 <devnoman01@gmail.com>
2022-07-01 08:22:32 +0600

    component load with dynamic data in todo route page complete

--- src/Pages/ToDo/ToDo.js
+++ src/Pages/ToDo/ToDo.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ToDoTaskItem from "../../Components/ToDoTaskItem/ToDoTaskItem";
 
 const ToDo = () => {
-  return <div></div>;
+  const [toDoTasks, setToDoTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setToDoTasks(data);
+      });
+  }, []);
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="container mx-auto px-3 lg:px-0">
+        <h1 className="text-3xl text-center font-medium pt-7 mb-0">
+          To-Do Tasks
+        </h1>
+        {/* todo task section */}
+        <div className="p-5 max-w-xl mx-auto rounded-lg">
+          <div className="my-3">
+            {toDoTasks.map((task) => (
+              <ToDoTaskItem key={task.id} task={task} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ToDo;
